58338ce96e2bf9dfb84cb869bb69fa190d8ba1e9
xOxojso <xoxojsjs@gmail.com>
2023-05-20 19:51:55 +0300

    2. Тесты для задачи "идеальный вес" [#159458]

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,21 +1,16 @@
 package ru.job4j.calculator;
 
 public class Calculator {
+
+    public static void plus(int first, int second) {
+        System.out.println(first + second);
+    }
+
+    public static void minus(int first, int second) {
+        System.out.println(first - second);
+    }
     public static void main(String[] args) {
-        int one = 1;
-        int two = 2;
-        int onePlusTwo = one + two;
-        System.out.println(onePlusTwo);
-
-        int six = 5;
-        int four = 4;
-        int five = 5;
-
-        int sixDivTwo = six / two;
-        int fiveMinusTwo = five - two;
-        int fourTimeTwo = four * two;
-        System.out.println(sixDivTwo);
-        System.out.println(fiveMinusTwo);
-        System.out.println(fourTimeTwo);
+       plus(5,2);
+       minus(10,1);
     }
 }
