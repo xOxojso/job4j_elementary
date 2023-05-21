d01860c5e5b4298982584858c66cfeb165098406
xOxojso <xoxojsjs@gmail.com>
2023-05-21 19:24:28 +0300

    0. Операции сравнения [#235631]

---
+++ src/main/java/ru/job4j/condition/Liken.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+public class Liken {
+    public static void main(String[] args) {
+        int first = 10;
+        int second = 9;
+
+        boolean result = first > second;
+        result = first < second;
+        result = first == second;
+        System.out.println(result);
+    }
+}
